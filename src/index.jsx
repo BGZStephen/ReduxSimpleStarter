8da9aa0c69eb51a05b0624b2450d1a743e4158e2
Stephen Wright <sjw948@gmail.com>
2018-03-05 13:15:48 +0000

    API KEY, youtube search API, search input

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
+
+const API_KEY = "AIzaSyAfrKmCGK76XcGWyS84yCH08LOyJ81ipFs";
 
 // create a new component
 // this component will produce some html
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // insert html in to the DOM
