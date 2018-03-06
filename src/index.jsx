303789bb3d4f0bf2d2a5a9d1021e38c5104644f2
Stephen Wright <sjw948@gmail.com>
2018-03-06 21:33:52 +0000

    activeVideo setter functionality

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
 import YTSearch from 'youtube-api-search';
 
@@ -11,10 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      activeVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos})
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      })
     });
   };
 
@@ -22,10 +29,18 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
       </div>
     );
   };
+
+  setActiveVideo(video) {
+    this.setState({selectedVideo: video});
+  }
 };
 
 ReactDOM.render(<App></App>, document.querySelector('.container'));
