956efd1b91a001752eb9d6c91e1642e181152a84
Stephen Wright <sjw948@gmail.com>
2018-03-06 20:50:04 +0000

    nest video-list-item within video-list + display search results

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = "AIzaSyAfrKmCGK76XcGWyS84yCH08LOyJ81ipFs";
-
 
 class App extends Component {
   constructor(props) {
@@ -15,15 +15,17 @@ class App extends Component {
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({videos})
-    })
-  }
+    });
+  };
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 ReactDOM.render(<App></App>, document.querySelector('.container'));
