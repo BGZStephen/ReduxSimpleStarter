198669a132a667201ac7b47dc7094e7888517663
Stephen Wright <sjw948@gmail.com>
2018-03-06 08:54:23 +0000

    state management for input + state setting onChange

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+  }
   render() {
-    return <input />
+    return (
+      <div>
+        <input onChange={this.onInputChange}/>
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
+  onInputChange = (event) => {
+    this.setState({term: event.target.value})
   }
 }
 
